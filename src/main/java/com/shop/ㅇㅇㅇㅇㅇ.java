8a72a6d090ed5fab1903cfda15edc7b4eed78558
crongd <leejaeho1500@gmail.com>
2024-02-02 00:06:00 +0900

    팰린드롬 씨x...

--- src/main/java/com/shop/ㅇㅇㅇㅇㅇ.java
+++ src/main/java/com/shop/ㅇㅇㅇㅇㅇ.java
@@ -1,40 +1,85 @@
 package com.shop;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.security.core.parameters.P;
+
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ㅇㅇㅇㅇㅇ {
-    public static int[] sol(String s) {
-//        if (arr.length == 1) {
-//            return new int[]{-1};
-//        }
-//        int[] result = new int[arr.length-1];
-//        Integer[] arr2 = new Integer[arr.length];
-//        for (int i = 0; i < arr.length; i++) {
-//            arr2[i] = arr[i];
-//        }
-//
-//        Arrays.sort(arr2, Collections.reverseOrder());
-//
-//        for (int i = 0; i < arr.length-1; i++) {
-//            result[i] = arr[i];
-//        }
-//
-//        return result;
+//    static String a = "";
+    // 팰린드롬
+    public static String sol(String s) {
+
+        String result = "";
 
         for (int i = 0; i < s.length(); i++) {
             for (int j = i + 1; j < s.length() + 1; j++) {
-                System.out.println(s.substring(i, j));
+//                System.out.println(s.substring(i, j));
+                String val = s.substring(i, j);
+                System.out.println("val:" + val);
+                System.out.println(i + " " + j);
+                if (val.length() <= 1) {
+                    System.out.println("val가 이거라 continue: " + val);
+                    continue;
+                }
+                for (int k = 0; k < val.length()/2; k++) {
+                    // val
+                    char first = val.charAt(k);
+                    char last = val.charAt((val.length()-1) -k);
+                    System.out.println("first: " + first);
+                    System.out.println("last: " + last);
+                    if (first != last) {
+                        System.out.println("같지 않아서 break");
+                        continue;
+                    }
+                    if (val.length() > result.length()) {
+                        result = val;
+                    }
+                }
+                System.out.println("result: " + result);
             }
         }
-        return null;
+
+        System.out.println("result: " + result);
+
+        return result;
+
+    }
+
+    static boolean check(String val) {
+        for (int i = 0; i < val.length()/2; i++) {
+            char first = val.charAt(i);
+            char last = val.charAt(val.length() -1 - i);
+            if (first != last) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println("abcdedcba");
+        System.out.println(sol("abcbabc"));
+//        if (val.length() % 2 == 0) { //짝수면
+//
+//        } else { //홀수면
+//
+//        }
+
+//        String a = "abcdedcbsaa";
+//
+//        for (int i = 0; i < a.length() / 2; i++) {
+//            char q = a.charAt(i);
+//            char w = a.charAt(a.length()-1 -i);
+//            if (q != w) {
+//                System.out.println(q +"와"+ w + "같지 않음");
+//                break;
+//            }
+//            System.out.println(q +"와"+ w + "같음");
+//        }
+
+//        System.out.println(a.substring(4, 4));
+//        a.substring()
+//        System.out.println(a.length()/2);
     }
 }
